39b1ea169aa3e731cd539cec26df998f8da19fb4
danyaq <daniil.orlov16@yandex.ru>
2021-07-09 13:04:39 +0300

    Initial Commit

---
+++ AnotherRealm/AnotherRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherRealmApp.swift
+//  AnotherRealm
+//
+//  Created by Daniil on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnotherRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
